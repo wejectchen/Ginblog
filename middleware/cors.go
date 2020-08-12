28fd208acf57d93ce8136a0de3c441cfc248bd9c
wejectchan <weject@gmail.com>
2020-08-13 01:44:31 +0800

    fix cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -3,22 +3,19 @@ package middleware
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func Cors() gin.HandlerFunc {
-
-	return cors.Default()
-	//return cors.New(cors.Config{
-	//	AllowAllOrigins: true,
-	//	AllowOrigins:  []string{"*"},
-	//	AllowMethods:  []string{"*"},
-	//	AllowHeaders:  []string{"Origin"},
-	//	//ExposeHeaders: []string{"ContentType", "Authorization"},
-	//	//AllowCredentials: true,
-	//	//AllowOriginFunc: func(origin string) bool {
-	//	//	return origin == "https://github.com"
-	//	//},
-	//	MaxAge: 12 * time.Hour,
-	//})
+	return cors.New(
+		cors.Config{
+			AllowAllOrigins:  true,
+			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowHeaders:     []string{"*"},
+			ExposeHeaders:    []string{"Content-Length", "Authorization", "Content-Type"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		},
+	)
 
 }
