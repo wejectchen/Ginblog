205474fb48b5aea73e6ed4e5d5789784049751c7
wejectchan <weject@gmail.com>
2020-08-15 01:23:19 +0800

    fix

--- web/admin/src/components/admin/Nav.vue
+++ web/admin/src/components/admin/Nav.vue
@@ -4,30 +4,34 @@
       <span>{{ collapsed ? 'Blog' : 'My Blog' }}</span>
     </div>
 
-    <a-menu
-      theme="dark"
-      mode="inline"
-      @click="goToPage"
-      :default-selected-keys="['index']"
-    >
-      <a-menu-item key="index"
-        ><a-icon type="dashboard"></a-icon><span>仪表盘</span></a-menu-item
-      >
+    <a-menu theme="dark" mode="inline" @click="goToPage" :default-selected-keys="['index']">
+      <a-menu-item key="index">
+        <a-icon type="dashboard"></a-icon>
+        <span>仪表盘</span>
+      </a-menu-item>
+
       <a-sub-menu>
-        <span slot="title"><a-icon type="file" /><span>文章管理</span></span>
-        <a-menu-item key="addart"
-          ><a-icon type="form" /><span>写文章</span></a-menu-item
-        >
-        <a-menu-item key="artlist"
-          ><a-icon type="snippets" /><span>文章列表</span></a-menu-item
-        >
+        <span slot="title">
+          <a-icon type="file" />
+          <span>文章管理</span>
+        </span>
+        <a-menu-item key="addart">
+          <a-icon type="form" />
+          <span>写文章</span>
+        </a-menu-item>
+        <a-menu-item key="artlist">
+          <a-icon type="snippets" />
+          <span>文章列表</span>
+        </a-menu-item>
       </a-sub-menu>
-      <a-menu-item key="catelist"
-        ><a-icon type="book" /><span>分类列表</span></a-menu-item
-      >
-      <a-menu-item key="userlist"
-        ><a-icon type="user" /><span>用户列表</span></a-menu-item
-      >
+      <a-menu-item key="catelist">
+        <a-icon type="book" />
+        <span>分类列表</span>
+      </a-menu-item>
+      <a-menu-item key="userlist">
+        <a-icon type="user" />
+        <span>用户列表</span>
+      </a-menu-item>
     </a-menu>
   </a-layout-sider>
 </template>
@@ -36,14 +40,14 @@
 export default {
   data() {
     return {
-      collapsed: false
+      collapsed: false,
     }
   },
   methods: {
     goToPage(item) {
       this.$router.push(item.key)
-    }
-  }
+    },
+  },
 }
 </script>
 
--- web/admin/src/views/Login.vue
+++ web/admin/src/views/Login.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="container">
     <div class="loginBox">
-      <a-form-model
-        ref="loginFormRef"
-        :rules="rules"
-        :model="formdata"
-        class="loginForm"
-      >
+      <a-form-model ref="loginFormRef" :rules="rules" :model="formdata" class="loginForm">
         <a-form-model-item prop="username">
           <a-input v-model="formdata.username" placeholder="请输入用户名">
             <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
@@ -25,12 +20,8 @@
         </a-form-model-item>
 
         <a-form-model-item class="loginBtn">
-          <a-button type="primary" style="margin:10px" @click="login"
-            >登录</a-button
-          >
-          <a-button type="info" style="margin:10px" @click="resetForm"
-            >取消</a-button
-          >
+          <a-button type="primary" style="margin:10px" @click="login">登录</a-button>
+          <a-button type="info" style="margin:10px" @click="resetForm">取消</a-button>
         </a-form-model-item>
       </a-form-model>
     </div>
@@ -43,7 +34,7 @@ export default {
     return {
       formdata: {
         username: '',
-        password: ''
+        password: '',
       },
       rules: {
         username: [
@@ -52,8 +43,8 @@ export default {
             min: 4,
             max: 12,
             message: '用户名必须在4到12个字符之间',
-            trigger: 'blur'
-          }
+            trigger: 'blur',
+          },
         ],
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
@@ -61,10 +52,10 @@ export default {
             min: 6,
             max: 20,
             message: '密码必须在6到20个字符之间',
-            trigger: 'blur'
-          }
-        ]
-      }
+            trigger: 'blur',
+          },
+        ],
+      },
     }
   },
   methods: {
@@ -72,15 +63,15 @@ export default {
       this.$refs.loginFormRef.resetFields()
     },
     login() {
-      this.$refs.loginFormRef.validate(async valid => {
+      this.$refs.loginFormRef.validate(async (valid) => {
         if (!valid) return this.$message.error('输入非法数据，请重新输入')
         const { data: res } = await this.$http.post('login', this.formdata)
         if (res.status != 200) return this.$message.error(res.message)
         window.sessionStorage.setItem('token', res.token)
         this.$router.push('admin')
       })
-    }
-  }
+    },
+  },
 }
 </script>
 
