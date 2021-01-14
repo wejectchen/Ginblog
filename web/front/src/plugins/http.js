fd33e0d3d7e4a094945b24aa01a91489dc333583
wejectchan <weject@gmail.com>
2021-01-14 17:17:59 +0800

    test

--- web/front/src/plugins/http.js
+++ web/front/src/plugins/http.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://47.108.116.119/:3000/api/v1'
+axios.defaults.baseURL = 'http://47.108.116.119:3000/api/v1'
 Vue.prototype.$http = axios
