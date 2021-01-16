c123ab7b38c84c98df1c55e96c8faf6f97f26b13
wejectchan <wejectchan>
2021-01-16 22:13:53 +0800

    fix footer

--- web/front/src/components/Footer.vue
+++ web/front/src/components/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <v-footer padless color="indigo darken-2">
-    <v-col class="text-center white--text">{{new Date().getFullYear()}} - GinBlog</v-col>
+    <v-row justify="center" no-gutters>
+      <v-col class="py-4 text-center white--text" cols="12"
+        >{{ new Date().getFullYear() }} - GinBlog
+      </v-col>
+
+      <v-col cols="12" class="py-2 text-center white--text">
+        蜀ICP备19035448号
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
